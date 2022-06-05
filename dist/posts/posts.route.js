68177a72518f906a2016d4931ea2ccb7e93bcd34
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-06-05 14:53:12 +0900

    feat: 라우터 경로 수정

--- dist/posts/posts.route.js
+++ dist/posts/posts.route.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var posts_service_1 = require("./posts.service");
 var router = (0, express_1.Router)();
-router.get('/vlog', posts_service_1.getVelogPosts);
+router.get('/velog', posts_service_1.getVelogPosts);
 router.get('/yozma', posts_service_1.getYozmPosts);
 router.get('/spiceworks', posts_service_1.getSpiceWorksPosts);
 exports.default = router;
--- src/posts/posts.route.ts
+++ src/posts/posts.route.ts
@@ -3,7 +3,7 @@ import { getVelogPosts, getYozmPosts, getSpiceWorksPosts } from './posts.service
 
 const router = Router();
 
-router.get('/vlog', getVelogPosts);
+router.get('/velog', getVelogPosts);
 router.get('/yozma', getYozmPosts);
 router.get('/spiceworks', getSpiceWorksPosts);
 
